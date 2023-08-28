13f591c758ab9edd31373addd64160e4033ca22f
UnkleWales <kadwales@gmail.com>
2023-08-28 13:14:03 +0100

    basic configs added

---
+++ QuizApp/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizApp.Models;
+
+namespace QuizApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<UserQuizResult> UserQuizResults { get; set; }
+        public DbSet<Quiz> Quizes { get; set; }
+        public DbSet<Question> Questions { get; set; }
+    }
+}
--- QuizApp/Program.cs
+++ QuizApp/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using QuizApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
